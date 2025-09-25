82aa730243c60eb0a703dbfaacbfa62cfececdcf
Adrian Enis <adrianenis2000gmail.com>
2025-09-25 17:26:02 -0400

    separating the routes

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,6 @@
+import {router} from "./router"
 import express, { Express } from "express"
 
-const server: Express = express()
+export const server: Express = express()
 
-// Routing
-
-server.get("/", (req, res) => {
-
-  res.json("Desde get")
-
-})
-server.post("/", (req, res) => {
-
-  res.json("Desde post")
-
-})
-server.put("/", (req, res) => {
-
-  res.json("Desde put")
-
-})
-server.patch("/", (req, res) => {
-
-  res.json("Desde patch")
-
-})
-server.delete("/", (req, res) => {
-
-  res.json("Desde delete")
-
-})
-
-
-export default server
+server.use("/api", router)
