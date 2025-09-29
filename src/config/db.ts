e01737a5897e67b598eb28a234fa09e256be2ce0
Adrian Enis <adrianenis2000gmail.com>
2025-09-29 18:09:26 -0400

    environment variables

---
+++ src/config/db.ts
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = new Sequelize(process.env.DATABASE_URL,{
+    dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+
+
+});
+
+export default db;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,22 @@
 import {router} from "./router"
 import express, { Express } from "express"
+import db from "./config/db"
 
+
+//connect Data base
+async function connectDB() {
+    try {
+        await db.authenticate(); // Verifica que la conexión con la DB sea válida
+        db.sync(); // Crea las tablas si no existen, según los modelos definidos
+        console.log('conexion exitosa');
+    } catch (error) {
+        console.log(error);
+        console.log('Hubo un error al conectar la base de dato');
+    }
+}
+connectDB();
 export const server: Express = express()
 
-server.use("/api", router)
+
+
+server.use("/api/products", router)
